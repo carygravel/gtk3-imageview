1771f13c1dda3bad65f331e8327c40fb1282ac56
Alexey Sokolov <sokolov@google.com>
2021-05-15 18:15:00 +0000

    Use int(x+0.5) instead round(x)
    Fix #23

--- lib/Gtk3/ImageView/Tool/Selector.pm
+++ lib/Gtk3/ImageView/Tool/Selector.pm
@@ -3,7 +3,6 @@ package Gtk3::ImageView::Tool::Selector;
 use warnings;
 use strict;
 use base 'Gtk3::ImageView::Tool';
-use POSIX qw(round);
 use Glib qw(TRUE FALSE);    # To get TRUE and FALSE
 use List::Util qw(min);
 use Readonly;
@@ -121,10 +120,10 @@ sub _update_selection {
       $self->view->to_image_coords( min( $x, $x2 ), min( $y, $y2 ) );
     $self->view->set_selection(
         {
-            x      => round($x),
-            y      => round($y),
-            width  => round($w),
-            height => round($h)
+            x      => int( $x + 0.5 ),
+            y      => int( $y + 0.5 ),
+            width  => int( $w + 0.5 ),
+            height => int( $h + 0.5 )
         }
     );
     return;
